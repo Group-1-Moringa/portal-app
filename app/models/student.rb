e5a5f63cc4120189aaa954a6bc5eb4c8881c3adf
Mark Chweya <chweyamark@gmail.com>
2023-07-03 21:39:06 +0300

    Update of the models/student.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,17 @@
 class Student < ApplicationRecord
-end
+    belongs_to :course
+    belongs_to :instructor
+  
+    # Add additional attributes or validations 
+    validates :name, presence: true
+    validates :grades, presence: true
+  
+    def technical_mentor_name
+      instructor.name if instructor
+    end
+  
+    def technical_mentor_email
+      instructor.email if instructor
+    end
+  end
+  
