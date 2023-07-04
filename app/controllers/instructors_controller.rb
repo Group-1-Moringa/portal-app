e35a7e5c1c2d50e14ffcc994d6c59362ec1f0c61
Mark Chweya <chweyamark@gmail.com>
2023-07-04 10:14:48 +0300

    update of instructors_controller.rb

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,2 +1,39 @@
 class InstructorsController < ApplicationController
-end
+    def index
+      @instructors = Instructor.all
+    end
+    def show
+      @instructor = Instructor.find(params[:id])
+    end
+    def new
+      @instructor = Instructor.new
+    end
+    def create
+      @instructor = Instructor.new(instructor_params)
+      if @instructor.save
+        redirect_to @instructor, notice: 'Instructor was successfully created.'
+      else
+        render :new
+      end
+    end
+    def edit
+      @instructor = Instructor.find(params[:id])
+    end
+    def update
+      @instructor = Instructor.find(params[:id])
+      if @instructor.update(instructor_params)
+        redirect_to @instructor, notice: 'Instructor was successfully updated.'
+      else
+        render :edit
+      end
+    end
+    def destroy
+      @instructor = Instructor.find(params[:id])
+      @instructor.destroy
+      redirect_to instructors_url, notice: 'Instructor was successfully destroyed.'
+    end
+    private
+    def instructor_params
+      params.require(:instructor).permit(:name, :email, :password)
+    end
+  end
