e4724fe1cea96973f74f06e71478bab585e5e018
Mark Chweya <chweyamark@gmail.com>
2023-07-04 10:55:59 +0300

    update last version of backend

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-    # Include devise modules
-    devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :validatable
-    # Custom associations
-    belongs_to :admin, optional: true
-    belongs_to :instructor, optional: true
-    belongs_to :student, optional: true
-  end
